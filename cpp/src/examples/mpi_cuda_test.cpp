3f6e668adddb2894da28224132e355bb72ec2aa8
Ahmet Uyar <ahmetuyar@gmail.com>
2021-02-08 11:40:06 +0300

    improved all-to-all messaging in mpi-cuda example

--- cpp/src/examples/mpi_cuda_test.cpp
+++ cpp/src/examples/mpi_cuda_test.cpp
@@ -13,26 +13,14 @@
 #include <net/buffer.hpp>
 #include <cuda_runtime.h>
 
-class CUDABuffer : public cylon::Buffer {
-public:
-    int64_t GetLength() override {
-        return length;
-    }
-    uint8_t * GetByteBuffer() override {
-        return buf;
-    }
-    CUDABuffer(uint8_t *buf, int64_t length) : buf(buf), length(length) {}
-private:
-    uint8_t *buf;
-    int64_t length;
-};
+int myrank = -1;
 
 class CUDAAllocator : public cylon::Allocator {
 public:
     cylon::Status Allocate(int64_t length, std::shared_ptr<cylon::Buffer> *buffer) override {
         uint8_t *b;
         cudaMallocManaged(&b, length * sizeof(char));
-        *buffer = std::make_shared<CUDABuffer>(b, length);
+        *buffer = std::make_shared<cylon::DefaultBuffer>(b, length);
         return cylon::Status::OK();
     }
 };
@@ -43,39 +31,28 @@ class RCB : public cylon::ReceiveCallback {
 public:
    /**
     * This function is called when a data is received
-    * @param source the source
-    * @param buffer the buffer allocated by the system, we need to free this
-    * @param length the length of the buffer
-    * @return true if we accept this buffer
     */
-    bool onReceive(int source, std::shared_ptr<cylon::Buffer> buffer, int length) {
+   bool onReceive(int source, std::shared_ptr<cylon::Buffer> buffer, int length) {
        std::string message;
        message.append(reinterpret_cast<const char*>(buffer->GetByteBuffer()));
-        LOG(INFO) << "received a buffer through onReceive with length: " << length << ", " << message;
-     return true;
-    }
+       LOG(INFO) << myrank << ", Received a buffer: " << message;
+       return true;
+   }
 
     /**
      * Receive the header, this happens before we receive the actual data
-     * @param source the source
-     * @param buffer the header buffer, which can be 6 integers
-     * @param length the length of the integer array
-     * @return true if we accept the header
      */
     bool onReceiveHeader(int source, int finished, int *buffer, int length) {
-        LOG(INFO) << "received a header with length: " << length;
+//        LOG(INFO) << "received a header with length: " << length;
         return true;
     }
 
     /**
      * This method is called after we successfully send a buffer
-     * @param target
-     * @param buffer
-     * @param length
      * @return
      */
     bool onSendComplete(int target, void *buffer, int length) {
-        LOG(INFO) << "called onSendComplete with length: " << length << " for the target: " << target;
+//        LOG(INFO) << "called onSendComplete with length: " << length << " for the target: " << target;
         return true;
     }
 
@@ -86,7 +63,7 @@ int main(int argc, char *argv[]) {
     auto start_start = std::chrono::steady_clock::now();
     auto mpi_config = std::make_shared<cylon::net::MPIConfig>();
     auto ctx = cylon::CylonContext::InitDistributed(mpi_config);
-    int myrank = ctx->GetRank();
+    myrank = ctx->GetRank();
 
     LOG(INFO) << "myrank: "  << myrank << ", world size: " << ctx->GetWorldSize();
 
@@ -97,7 +74,7 @@ int main(int argc, char *argv[]) {
 
     RCB * rcb = new RCB();
     CUDAAllocator allocator{};
-    std::shared_ptr<cylon::Buffer> buffer = std::make_shared<cylon::DefaultBuffer>(nullptr, 0);
+    std::shared_ptr<cylon::Buffer> buffer;
     cylon::Status stat = allocator.Allocate(20, &buffer);
     if (!stat.is_ok()) {
         LOG(FATAL) << "Failed to allocate buffer with length " << 20;
@@ -106,25 +83,25 @@ int main(int argc, char *argv[]) {
     std::shared_ptr<cylon::AllToAll> all =
             std::make_shared<cylon::AllToAll>(ctx, allWorkers, allWorkers, ctx->GetNextSequence(), rcb, &allocator);
 
-    char message[20];
-    using namespace std::chrono_literals;
-    if (myrank == 0) {
-        strcpy(message, "Hello, there");
-        all->insert(message, strlen(message) + 1, 1);
-        all->insert(message, strlen(message) + 1, 1);
-    }
+    std::string message("hello from ");
+    message += std::to_string(myrank);
 
-    if (myrank == 1) {
-        strcpy(message, "Hello, there ..");
-        all->insert(message, strlen(message) + 1, 0);
-        all->insert(message, strlen(message) + 1, 0);
+    char msg[20];
+    strcpy(msg, message.c_str());
+    using namespace std::chrono_literals;
+    for (int wID: allWorkers) {
+        all->insert(msg, message.length() + 1, wID);
     }
 
     all->finish();
 
+    int i = 1;
     while(!all->isComplete()) {
-        LOG(INFO) << myrank <<  ", has not completed yet.";
-        std::this_thread::sleep_for(1000ms);
+        if (i % 10 == 0) {
+            LOG(INFO) << myrank << ", has not completed yet.";
+            std::this_thread::sleep_for(1000ms);
+        }
+        i++;
     }
 
     ctx->Finalize();
