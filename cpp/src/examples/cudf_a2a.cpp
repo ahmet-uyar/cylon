59f7f1afeee238b9b535a7dde6851d518ea26e6b
Ahmet Uyar <ahmetuyar@gmail.com>
2021-02-19 12:52:32 +0300

    implemented initial version of CudfAllToAll

--- cpp/src/examples/cudf_a2a.cpp
+++ cpp/src/examples/cudf_a2a.cpp
@@ -29,6 +29,48 @@
 int myrank = -1;
 
 //////////////////////////////////////////////////////////////////////
+// global types and fuctions
+//////////////////////////////////////////////////////////////////////
+// sizes of cudf types in tables
+// ref: type_id in cudf/types.hpp
+int type_bytes[] = {0, 1, 2, 4, 8, 1, 2, 4, 8, 4, 8, 1, 4, 8, 8, 8, 8, 4, 8, 8, 8, 8, -1, -1, -1, 4, 8, -1, -1};
+
+/**
+ * whether the data type is uniform in size such as int (4 bytes) or string(variable length)
+ * @param input
+ * @return
+ */
+int data_type_size(cudf::column_view const& cw){
+    return type_bytes[static_cast<int>(cw.type().id())];
+}
+
+/**
+ * whether the data type is uniform in size such as int (4 bytes) or string(variable length)
+ * @param input
+ * @return
+ */
+bool uniform_size_data(cudf::column_view const& cw){
+    int dataTypeSize = data_type_size(cw);
+    return dataTypeSize == -1 ? false : true;
+}
+
+/**
+ * data buffer length of a column in bytes
+ * @param input
+ * @return
+ */
+cudf::size_type dataLength(cudf::column_view const& cw){
+    int elementSize = type_bytes[static_cast<int>(cw.type().id())];
+    if (elementSize == -1) {
+        std::cout << "ERRORRRRRR unsupported type id: " << static_cast<int>(cw.type().id()) << std::endl;
+        return -1;
+    }
+
+    // even null values exist in the buffer with unspecified values
+    return elementSize * cw.size();
+}
+
+//////////////////////////////////////////////////////////////////////
 // CudfBuffer implementations
 //////////////////////////////////////////////////////////////////////
 CudfBuffer::CudfBuffer(std::shared_ptr<rmm::device_buffer> rmmBuf) : rmmBuf(std::move(rmmBuf)) {}
@@ -63,7 +105,168 @@ cylon::Status CudfAllocator::Allocate(int64_t length, std::shared_ptr<cylon::Buf
 //////////////////////////////////////////////////////////////////////
 // CudfAllToAll implementations
 //////////////////////////////////////////////////////////////////////
-CudfAllToAll::CudfAllToAll() : data_types() {}
+CudfAllToAll::CudfAllToAll(std::shared_ptr<cylon::CylonContext> &ctx,
+                           const std::vector<int> &sources,
+                           const std::vector<int> &targets,
+                           int edgeId,
+                           CudfCallback callback) :
+    sources_(sources),
+    targets_(targets),
+    recv_callback_(std::move(callback)),
+    completed_(false),
+    finishCalled_(false){
+
+    allocator_ = new CudfAllocator();
+
+    // we need to pass the correct arguments
+    all_ = std::make_shared<cylon::AllToAll>(ctx, sources_, targets_, edgeId, this, allocator_);
+
+    // add the trackers for sending
+    for (auto t : targets_) {
+        inputs_.insert(std::make_pair(t, std::make_shared<PendingSends>()));
+    }
+
+    for (auto t : sources_) {
+        receives_.insert(std::make_pair(t, std::make_shared<PendingReceives>()));
+    }
+}
+
+int CudfAllToAll::insert(const std::shared_ptr<cudf::table> &table, int32_t target) {
+    return insert(table, target, -1);
+}
+
+int CudfAllToAll::insert(const std::shared_ptr<cudf::table> &table,
+                          int32_t target,
+                          int32_t reference) {
+    // todo: check weather we have enough memory
+    // lets save the table into pending and move on
+    inputs_[target]->tableQueue.push(std::make_pair(table, reference));
+    return 1;
+}
+
+bool CudfAllToAll::isComplete() {
+
+    for (const auto &t : inputs_) {
+      std::pair<std::shared_ptr<cudf::table>, int32_t> currentPair = t.second->tableQueue.front();
+      t.second->tableQueue.pop();
+      insertTableToA2A(currentPair.first, t.first, currentPair.second);
+    }
+
+    return all_->isComplete();
+}
+
+bool CudfAllToAll::insertTableToA2A(std::shared_ptr<cudf::table> table, int target, int ref) {
+    // construct header message to send
+    // for all columns, send data-type, whether it has null data and offsets buffer.
+    // header data
+//    int columns = table->num_columns();
+    int columns = 2;
+    int headersLength = 3;
+    int * tableHeaders = new int[headersLength];
+    tableHeaders[0] = 0; // shows it is a table header. todo: make it enum
+    tableHeaders[1] = ref;
+    tableHeaders[2] = columns;
+    all_->insert(nullptr, 0, target, tableHeaders, headersLength);
+
+    for (int i = 0; i < columns; ++i) {
+        insertColumnToA2A(table->get_column(i), i, target);
+    }
+
+    return true;
+}
+
+bool CudfAllToAll::insertColumnToA2A(cudf::column &clmn, int columnIndex, int target) {
+
+    cudf::column_view cw = clmn.view();
+    int headersLength = 6;
+    int * columnHeaders = new int[headersLength];
+    columnHeaders[0] = 1; // it is a column header. todo: make it enum
+    columnHeaders[1] = columnIndex;
+    columnHeaders[2] = (int)(cw.type().id()); // data-type of the column
+    // whether the column has null array, 1 has the null buffer, 0 means no null buffer
+    if (cw.nullable())
+        columnHeaders[3] = 1;
+    else
+        columnHeaders[3] = 0;
+
+    //whether the column has offsets array
+    if (uniform_size_data(cw))
+        columnHeaders[4] = 0;
+    else
+        columnHeaders[4] = 1;
+
+    columnHeaders[5] = clmn.size();
+
+    // send data
+    const uint8_t *dataBuffer = cw.data<uint8_t>();
+    int dataLen = dataLength(cw);
+    all_->insert(dataBuffer, dataLen, target, columnHeaders, headersLength);
+
+    // send null buffer
+    if (cw.nullable()) {
+        uint8_t * nullBuffer = (uint8_t *)cw.null_mask();
+        double dataTypeSize = data_type_size(cw);
+        int nullBufSize = ceil(cw.size() / dataTypeSize);
+        all_->insert(nullBuffer, nullBufSize, target);
+    }
+
+    return true;
+}
+
+void CudfAllToAll::finish() {
+    finished = true;
+    all_->finish();
+}
+
+void CudfAllToAll::close() {
+    // clear the input map
+    inputs_.clear();
+    // call close on the underlying all-to-all
+    all_->close();
+
+    delete allocator_;
+}
+
+void CudfAllToAll::constructColumn(std::shared_ptr<PendingReceives> pr) {
+
+    std::unique_ptr<cudf::column> column;
+
+    cudf::data_type dt(static_cast<cudf::type_id>(pr->columnDataType));
+    std::shared_ptr<rmm::device_buffer> dataBuffer = pr->dataBuffer;
+    std::shared_ptr<rmm::device_buffer> nullBuffer = pr->nullBuffer;
+
+    // if there is no null buffer or offset buffer, create the column with data only
+    if(!pr->hasNullBuffer) {
+        column = std::make_unique<cudf::column>(dt, pr->dataSize, *dataBuffer);
+    } else { //todo: handle offsets
+        column = std::make_unique<cudf::column>(dt, pr->dataSize, *dataBuffer, *nullBuffer);
+    }
+
+    // if the column is constructed, add it to the list
+    if(column) {
+        pr->columns.insert({pr->columnIndex, std::move(column)});
+
+        // clear column related data from pr
+        pr->columnIndex = -1;
+        pr->columnDataType = -1;
+        pr->dataSize = -1;
+        pr->dataBuffer.reset();
+        pr->nullBuffer.reset();
+        pr->hasNullBuffer = false;
+    }
+
+}
+
+std::shared_ptr<cudf::table> CudfAllToAll::constructTable(std::shared_ptr<PendingReceives> pr) {
+
+    auto columnVector = std::make_unique<std::vector<std::unique_ptr<cudf::column>>>();
+    for (int i=0; i < pr->columns.size(); i++) {
+        columnVector->push_back(std::move(pr->columns.at(i)));
+    }
+
+    std::shared_ptr<cudf::table> tbl = std::make_shared<cudf::table>(std::move(*columnVector));
+    return tbl;
+}
 
 /**
  * This function is called when a data is received
@@ -72,18 +275,47 @@ bool CudfAllToAll::onReceive(int source, std::shared_ptr<cylon::Buffer> buffer, 
   LOG(INFO) << "buffer received from the source: " << source;
   std::shared_ptr<CudfBuffer> cb = std::dynamic_pointer_cast<CudfBuffer>(buffer);
 
-  uint8_t *hostArray= new uint8_t[length];
-  cudaMemcpy(hostArray, cb->GetByteBuffer(), length, cudaMemcpyDeviceToHost);
+  // if the data buffer is not received yet, get it
+  std::shared_ptr<PendingReceives> pr = receives_.at(source);
+  if (!pr->dataBuffer) {
+    pr->dataBuffer = cb->getBuf();
 
-  if (data_types.at(source) == 3) {
-    int32_t * hdata = (int32_t *) hostArray;
-    LOG(INFO) << "==== data[0]: " << hdata[0] << ", data[1]: " << hdata[1];
-  } else if (data_types.at(source) == 4) {
-    int64_t *hdata = (int64_t *) hostArray;
-    LOG(INFO) << "==== data[0]: " << hdata[0] << ", data[1]: " << hdata[1];
+    // if there is no null buffer or offset buffer, create the column
+    if(!pr->hasNullBuffer) {
+        constructColumn(pr);
+    }
+  } else if(!pr->hasNullBuffer) {
+    pr->nullBuffer = cb->getBuf();
+    // if there is no offset buffer, create the column
+    constructColumn(pr);
   } else {
-    LOG(WARNING) << "Unrecognized data type ==== : " << data_types.at(source);
+      LOG(WARNING) << "although dataBuffer and nullBuffer have been received. a new buffer received from: " << source
+        << ", buffer length: " << length;
   }
+
+  // if all columns are created, create the table
+  if (pr->columns.size() == pr->numberOfColumns) {
+      std::shared_ptr<cudf::table> tbl = constructTable(pr);
+      recv_callback_(source, tbl, pr->reference);
+
+      // clear table data from pr
+      pr->columns.clear();
+      pr->numberOfColumns = -1;
+      pr->reference = -1;
+  }
+
+//  uint8_t *hostArray= new uint8_t[length];
+//  cudaMemcpy(hostArray, cb->GetByteBuffer(), length, cudaMemcpyDeviceToHost);
+//
+//  if (data_types.at(source) == 3) {
+//    int32_t * hdata = (int32_t *) hostArray;
+//    LOG(INFO) << "==== data[0]: " << hdata[0] << ", data[1]: " << hdata[1];
+//  } else if (data_types.at(source) == 4) {
+//    int64_t *hdata = (int64_t *) hostArray;
+//    LOG(INFO) << "==== data[0]: " << hdata[0] << ", data[1]: " << hdata[1];
+//  } else {
+//    LOG(WARNING) << "Unrecognized data type ==== : " << data_types.at(source);
+//  }
 
   return true;
 }
@@ -93,7 +325,19 @@ bool CudfAllToAll::onReceive(int source, std::shared_ptr<cylon::Buffer> buffer, 
  */
 bool CudfAllToAll::onReceiveHeader(int source, int finished, int *buffer, int length) {
   if (length > 0) {
-    data_types.insert({source, buffer[0]});
+      if (buffer[0] == 0) { // table header
+          std::shared_ptr<PendingReceives> pr = receives_.at(source);
+          pr->reference = buffer[1];
+          pr->numberOfColumns = buffer[2];
+          LOG(INFO) << "----received a table header from the source: " << source;
+      } else if(buffer[0] == 1){ // column header
+          std::shared_ptr<PendingReceives> pr = receives_.at(source);
+          pr->columnIndex = buffer[1];
+          pr->columnDataType = buffer[2];
+          pr->hasNullBuffer = buffer[3] == 0 ? false: true;
+          pr->hasOffsetBuffer = buffer[4] == 0 ? false: true;
+          pr->dataSize = buffer[5];
+      }
   }
   LOG(INFO) << "----received a header buffer with length: " << length;
   return true;
@@ -111,25 +355,6 @@ bool CudfAllToAll::onSendComplete(int target, const void *buffer, int length) {
 //////////////////////////////////////////////////////////////////////
 // test functions
 //////////////////////////////////////////////////////////////////////
-// sizes of cudf types in tables
-// ref: type_id in cudf/types.hpp
-int type_bytes[] = {0, 1, 2, 4, 8, 1, 2, 4, 8, 4, 8, 1, 4, 8, 8, 8, 8, 4, 8, 8, 8, 8, -1, -1, -1, 4, 8, -1, -1};
-
-/**
- * data buffer length of a column in bytes
- * @param input
- * @return
- */
-cudf::size_type dataLength(cudf::column_view const& input){
-    int elementSize = type_bytes[static_cast<int>(input.type().id())];
-    if (elementSize == -1) {
-        std::cout << "ERRORRRRRR unsupported type id: " << static_cast<int>(input.type().id()) << std::endl;
-        return -1;
-    }
-
-    // even null values exist in the buffer with unspecified values
-    return elementSize * input.size();
-}
 
 void testAllocator() {
     CudfAllocator allocator{};
@@ -190,12 +415,14 @@ int main(int argc, char *argv[]) {
     // set the gpu
     cudaSetDevice(myrank % numberOfGPUs);
 
-    CudfAllToAll * cA2A = new CudfAllToAll();
-    CudfAllocator * allocator = new CudfAllocator();
-    std::shared_ptr<cylon::AllToAll> all =
-            std::make_shared<cylon::AllToAll>(ctx, allWorkers, allWorkers, ctx->GetNextSequence(), cA2A, allocator);
+    // define call back to catch the receiving tables
+    CudfCallback callback = [=](int source, const std::shared_ptr<cudf::table> &table_, int reference) {
+        LOG(INFO) << "received a table ...................)))))))))))))))))))))))))))";
+        return true;
+    };
 
-    LOG(INFO) << myrank << ": after all-to-all init.";
+    CudfAllToAll * cA2A = new CudfAllToAll(ctx, allWorkers, allWorkers, 1, callback);
+    CudfAllocator * allocator = new CudfAllocator();
 
     // construct table
     std::string input_csv_file = argv[1];
@@ -204,34 +431,51 @@ int main(int argc, char *argv[]) {
     cudf::io::table_with_metadata ctable = cudf::io::read_csv(options);
     LOG(INFO) << myrank << ", number of columns: " << ctable.tbl->num_columns();
 
-    // column data
-    int columnIndex = myrank;
-    cudf::column_view cw = ctable.tbl->get_column(columnIndex).view();
-//    testColumnAccess(cw);
-    LOG(INFO) << myrank << ", column[" << columnIndex << "] size: " << cw.size();
-    const uint8_t *sendBuffer = cw.data<uint8_t>();
-    int dataLen = dataLength(cw);
-
-    // header data
-    int headerLen = 2;
-    int * headers = new int[headerLen];
-    headers[0] = (int)(cw.type().id());
-    headers[1] = myrank;
-
+//    std::shared_ptr<cudf::table> tbl = std::make_shared<cudf::table>(ctable.tbl);
     for (int wID: allWorkers) {
-        all->insert(sendBuffer, dataLen, wID, headers, headerLen);
+        cA2A->insert(std::move(ctable.tbl), wID);
     }
 
-    all->finish();
+    cA2A->finish();
 
     int i = 1;
-    while(!all->isComplete()) {
+    while(!cA2A->isComplete()) {
         if (i % 100 == 0) {
             LOG(INFO) << myrank << ", has not completed yet.";
             std::this_thread::sleep_for(std::chrono::seconds(1));
         }
         i++;
     }
+
+
+//    // column data
+//    int columnIndex = myrank;
+//    cudf::column_view cw = ctable.tbl->get_column(columnIndex).view();
+////    testColumnAccess(cw);
+//    LOG(INFO) << myrank << ", column[" << columnIndex << "] size: " << cw.size();
+//    const uint8_t *sendBuffer = cw.data<uint8_t>();
+//    int dataLen = dataLength(cw);
+//
+//    // header data
+//    int headerLen = 2;
+//    int * headers = new int[headerLen];
+//    headers[0] = (int)(cw.type().id());
+//    headers[1] = myrank;
+//
+//    for (int wID: allWorkers) {
+//        all->insert(sendBuffer, dataLen, wID, headers, headerLen);
+//    }
+
+//    all->finish();
+//
+//    int i = 1;
+//    while(!all->isComplete()) {
+//        if (i % 100 == 0) {
+//            LOG(INFO) << myrank << ", has not completed yet.";
+//            std::this_thread::sleep_for(std::chrono::seconds(1));
+//        }
+//        i++;
+//    }
 
     ctx->Finalize();
     return 0;
--- cpp/src/examples/cudf_a2a.hpp
+++ cpp/src/examples/cudf_a2a.hpp
@@ -38,10 +38,107 @@ public:
     cylon::Status Allocate(int64_t length, std::shared_ptr<cylon::Buffer> *buffer) override;
 };
 
+enum CudfHeader {
+    CUDF_HEADER_INIT = 0,
+    // wea are still sending the data about the column
+    CUDF_HEADER_COLUMN_CONTINUE = 1,
+    // this is the end of the column
+    CUDF_HEADER_COLUMN_END = 2
+};
+
+/**
+ * Keep track of the items to send for a target
+ */
+struct PendingSends {
+    // pending tables to be sent with it's reference
+    std::queue<std::pair<std::shared_ptr<cudf::table>, int32_t>> tableQueue{};
+
+    // state of the send
+    CudfHeader status = CUDF_HEADER_INIT;
+};
+
+struct PendingReceives {
+    // number of columns in the table
+    int numberOfColumns{};
+    // the reference
+    int reference{};
+
+    // data type of the column
+    int columnDataType{};
+    // the current data column index
+    int columnIndex{};
+    // whether the current column has the null buffer
+    bool hasNullBuffer{};
+    // whether the current column has the offset buffer
+    bool hasOffsetBuffer{};
+
+    // number of data elements
+    int dataSize{};
+
+
+    // data buffer for the current column
+    std::shared_ptr<rmm::device_buffer> dataBuffer;
+    // null buffer for the current column
+    std::shared_ptr<rmm::device_buffer> nullBuffer;
+    // offsets buffer for the current column
+    std::shared_ptr<rmm::device_buffer> offsetsBuffer;
+    // currently received columns
+    std::unordered_map<int, std::unique_ptr<cudf::column>> columns;
+};
+
+/**
+ * This function is called when a table is fully received
+ * @param source the source
+ * @param table the table that is received
+ * @param reference the reference number sent by the sender
+ * @return true if we accept this buffer
+ */
+using CudfCallback = std::function<bool(int source, const std::shared_ptr<cudf::table> &table, int reference)>;
+
 class CudfAllToAll : public cylon::ReceiveCallback {
 
 public:
-  CudfAllToAll();
+  CudfAllToAll(std::shared_ptr<cylon::CylonContext> &ctx,
+               const std::vector<int> &sources,
+               const std::vector<int> &targets,
+               int edgeId,
+               CudfCallback callback);
+
+  /**
+   * Insert a table to be sent, if the table is accepted return true
+   *
+   * @param table the table to send
+   * @param target the target to send the table
+   * @return true if the buffer is accepted
+   */
+  int insert(const std::shared_ptr<cudf::table> &table, int32_t target);
+
+  /**
+   * Insert a table to be sent, if the table is accepted return true
+   *
+   * @param table the table to send
+   * @param target the target to send the table
+   * @param reference a reference that can be sent in the header
+   * @return true if the buffer is accepted
+   */
+  int insert(const std::shared_ptr<cudf::table> &arrow, int32_t target, int32_t reference);
+
+  /**
+   * Check weather the operation is complete, this method needs to be called until the operation is complete
+   * @return true if the operation is complete
+   */
+  bool isComplete();
+
+  /**
+   * When this function is called, the operation finishes at both receivers and targets
+   * @return
+   */
+  void finish();
+
+  /**
+   * Close the operation
+   */
+  void close();
 
   /**
    * This function is called when a data is received
@@ -60,7 +157,53 @@ public:
   bool onSendComplete(int target, const void *buffer, int length) override;
 
 private:
-    std::unordered_map<int, int> data_types;
+    bool insertTableToA2A(std::shared_ptr<cudf::table> table, int target, int ref);
+    bool insertColumnToA2A(cudf::column &clmn, int columnIndex, int target);
+    void constructColumn(std::shared_ptr<PendingReceives> pr);
+    std::shared_ptr<cudf::table> constructTable(std::shared_ptr<PendingReceives> pr);
+
+    /**
+     * The sources
+     */
+    std::vector<int> sources_;
+
+    /**
+     * The targets
+     */
+    std::vector<int> targets_;
+
+    /**
+     * The underlying alltoall communication
+     */
+    std::shared_ptr<cylon::AllToAll> all_;
+
+    /**
+     * Keep track of the inputs
+     */
+    std::unordered_map<int, std::shared_ptr<PendingSends>> inputs_;
+
+    /**
+     * Keep track of the receives
+     */
+    std::unordered_map<int, std::shared_ptr<PendingReceives>> receives_;
+
+    /**
+     * this is the allocator to create memory when receiving
+     */
+    CudfAllocator * allocator_;
+
+    /**
+     * inform the callback when a table received
+     */
+    CudfCallback recv_callback_;
+
+    /**
+     * We have received the finish
+     */
+    bool finished = false;
+
+    bool completed_;
+    bool finishCalled_;
 };
 
 #endif //CYLON_CUDF_H
